55219f71a0d095986a014208c55b47e5d78d70e2
msaikat-git <123349228+msaikat-git@users.noreply.github.com>
2023-01-23 11:03:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 
 import streamlit;
 streamlit.title("My parents' new healthy diner");
+
+streamlit.header('Breakfast Menu');
+streamlit.text('Omega 3 and Blueberry Oatmeal');
+streamlit.text('Kale, Spinach and Rocket Smoothie');
+streamlit.text('Hard-Boiled free-range eggs');
