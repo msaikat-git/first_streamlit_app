ce291c412b8c1b964182e1f68890037286324a6a
msaikat-git <123349228+msaikat-git@users.noreply.github.com>
2023-01-23 11:24:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 
 import streamlit;
-import pandas;
+
 streamlit.title("My parents' new healthy diner");
 
 streamlit.header('Breakfast favorites');
@@ -9,5 +9,7 @@ streamlit.text('Kale, Spinach and Rocket Smoothie');
 streamlit.text('Hard-Boiled free-range eggs');
 
 streamlit.header('Build your own fruit smoothie');
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+import pandas;
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index));
 streamlit.dataframe(my_fruit_list);
